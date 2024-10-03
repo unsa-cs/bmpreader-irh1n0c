fc621eec96894dd5957ad06e774d33f3d6f3ca5b
irh1n0c <lhuaynasi@gmail.com>
2024-10-02 19:04:06 -0500

    image change gris

--- main.c
+++ main.c
@@ -5,39 +5,38 @@
 BMPImage *image;
 
 void display() {
-  glClear(GL_COLOR_BUFFER_BIT);
-  if (image) {
-    drawBMP(image);
-  }
-  glFlush();
+    glClear(GL_COLOR_BUFFER_BIT);
+    if (image) {
+        drawBMP(image);
+    }
+    glFlush();
 }
 
-int main() {
-  char filename[256];
+int main(int argc, char *argv[]) {
+    char filename[256];
 
-  // Pedir al usuario el nombre del archivo BMP
-  printf("Ingrese el nombre del archivo BMP (con extensión): ");
-  scanf("%255s", filename);
+    // Pedir al usuario el nombre del archivo BMP
+    printf("Ingrese el nombre del archivo BMP (con extensión): ");
+    scanf("%255s", filename);
 
-  image = readBMP(filename);
-  if (!image) return 1;
+    image = readBMP(filename);
+    if (!image) return 1;
 
-  // Inicializar GLUT
-  int argc = 1; // Necesario para evitar problemas con glutInit
-  char *argv[1] = { "" }; // Argumento vacío para GLUT
-  glutInit(&argc, argv);
+    // Inicializar GLUT
+    glutInit(&argc, argv);
+    
+    // Establecer el modo de visualización
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(image->width, image->height);
+    glutCreateWindow("Visualizador de BMP");
 
-  // Establecer el modo de visualización
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-  glutInitWindowSize(image->width, image->height);
-  glutCreateWindow("Visualizador de BMP");
+    glLoadIdentity();
+    glOrtho(0, image->width, image->height, 0, -1, 1);
 
-  glLoadIdentity();
-  glOrtho(0, image->width, image->height, 0, -1, 1);
+    glutDisplayFunc(display);
+    glutMainLoop();
 
-  glutDisplayFunc(display);
-  glutMainLoop();
-
-  freeBMP(image);
-  return 0;
+    // Liberar la memoria de la imagen después de salir del bucle principal
+    freeBMP(image);
+    return 0;
 }
